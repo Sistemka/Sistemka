509b6aa9cec8c6d47eb1bed2e29f958b9a23d84b
MarkAntipin <antipinsuperstar@yandex.ru>
2020-02-28 11:46:26 +0300

    add segmentator

---
+++ sistemka/services/Segmentator.py
@@ -0,0 +1,54 @@
+import os
+import zipfile
+from pathlib import Path
+
+from .base import BaseService
+
+
+class Segmentator(BaseService):
+    def __init__(self,
+                 service_name: str = 'Segmentator',
+                 url: str = 'http://165.22.95.38:4993/',
+                 **kwargs: dict
+                 ):
+        super(Segmentator, self).__init__(
+            service_name=service_name,
+            url=url,
+            **kwargs
+        )
+
+    def get_files(
+            self,
+            image_path: str,
+            cropped_images_dir: str,
+            **kwargs: dict
+            ):
+        url = self.url + 'segmentation'
+        image = open(image_path, 'rb')
+        files = {'image': image}
+        r = self.make_request(
+            request_type='POST',
+            url=url,
+            files=files,
+            params={'return_vector': False},
+            **kwargs
+        )
+        image.close()
+
+        Path(cropped_images_dir).mkdir(parents=True, exist_ok=True)
+
+        zip_file_path = f"{cropped_images_dir}.zip"
+
+        with open(zip_file_path, 'wb') as f:
+            f.write(r.content)
+        with zipfile.ZipFile(zip_file_path, "r") as z_f:
+            z_f.extractall(cropped_images_dir)
+
+        os.remove(zip_file_path)
+
+        cropped_images_paths = [
+            image for image in os.listdir(cropped_images_dir)
+            if os.path.isfile(Path(cropped_images_dir, image))
+        ]
+
+        return cropped_images_paths
--- sistemka/services/__init__.py
+++ sistemka/services/__init__.py
@@ -1,9 +1,11 @@
 from .SearchEngine import SearchEngine
 from .ImageManager import ImageManager
 from .Image2Vector import Image2Vector
+from .Segmentator import Segmentator
 
 __all__ = [
     'SearchEngine',
     'ImageManager',
-    'Image2Vector'
+    'Image2Vector',
+    'Segmentator'
 ]
